ff01b03482bffb52ca3a3a573437c05eeb58715e
Robert Walters <robbwalt95@gmail.com>
2021-12-23 16:28:31 -0700

    2.5 Lock Down - Look Up
    -default_js
    --updated lastUpdate
    --updated footer V#
    --added locked links code
    
    -Index
    --locked category literature
    --locked content Only Blinds
    --locked content Holdin' Out
    --locked content The Un
    --locked content Embark
    --Changed Note to 2.5 Start Date
    
    -Author
    --locked category literature
    
    -Music
    --locked content The Un
    --locked category literature
    
    -Playlist
    --locked category literature
    
    -Software
    --locked category literature
    
    -Bookstore
    --locked category literature
    
    -Chopsticks
    --locked category literature
    
    -MultiChoice
    --locked category literature
    
    -Pizza
    --locked category literature
    
    -PlaylistProject
    --locked category literature
    
    -RobbStat Project
    --Content Update about V2.5
    --locked category literature
    
    -SensorFlash
    --locked category literature

--- site/default_js.js
+++ site/default_js.js
@@ -1,21 +1,37 @@
+// Lock links
+const lnk = document.getElementById('locked');
+if (lnk) {
+
+    if (window.addEventListener) {
+        document.addEventListener('click', function (e) {
+            if (e.target.id === lnk.id) {
+                e.preventDefault();         // Comment this line to enable the link tag again.
+                alert("Page is currently locked. \n\nBypassing is simple. \nThink outside of the box."); // Comment this line to enable the link tag again.
+            }
+     });
+    }
+
+}
+
 
 // Set Footer String
-const footerEl = document.getElementById("footer")
-if (footerEl) footerEl.innerHTML ="Robb Walters - robbstat.tk - V2 - 2021.";
+const footerEl = document.getElementById("footer");
+if (footerEl) footerEl.innerHTML ="Robb Walters - robbstat.tk - V2.5 - 2021.";
 
 // Set Last Update String
-const lastUpdateEl = document.getElementById("lastUpdate")
-if (lastUpdateEl) lastUpdateEl.innerHTML ="Last Updated: 12/17/2021";
+const lastUpdateEl = document.getElementById("lastUpdate");
+if (lastUpdateEl) lastUpdateEl.innerHTML ="Last Updated: 12/23/2021";
 
 
 // Set Title Description String
-const titleDescrEl = document.getElementById("titleDescr")
+const titleDescrEl = document.getElementById("titleDescr");
 if (titleDescrEl) titleDescrEl.innerHTML ="Creating and Sharing";
 
 // Display resume doc download link or not
 var working = false;
 if (working == false) {
 	  var str = "--> Download Robb's Programming Resume! <--";
-	  var result = str.link("docs/Robb_CompSci_Resume.docx");
-      document.getElementById("resume").innerHTML = result;
+	  const result = str.link("docs/Robb_CompSci_Resume.docx");
+	  if(result) document.getElementById("resume").innerHTML = result;
 }
+
